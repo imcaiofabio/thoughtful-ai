329d368a0aaf1a161015642a29a44a38e4fc0389
caio <caio.fabio91@gmail.com>
2025-03-07 12:32:09 -0300

    Implementing data ingestion with csv file

--- sort_package.py
+++ sort_package.py
@@ -1,4 +1,8 @@
+from pandas import read_csv
+
 def sort(width, height, length, mass):
+    if not all(isinstance(x, (int, float)) and x >= 0 for x in [width, height, length, mass]):
+        raise ValueError("All dimensions and mass must be non-negative numbers.")
     volume = width * height * length
     is_bulky = volume >= 1_000_000 or max(width, height, length) >= 150
 
@@ -10,3 +14,19 @@ def sort(width, height, length, mass):
         return "SPECIAL"
     else:
         return "STANDARD"
+
+
+def ingest_data(file_path="/Users/caio/code/thoughtful-ai/packages.csv"):
+    df = read_csv(file_path, on_bad_lines='skip')
+    df_list = df.values.tolist()
+
+    sorted_packages = []
+    for d in df_list:
+        w, h, l, m = d
+        try:
+            r = sort(float(w), float(h), float(l), float(m))
+            d.append(r)
+            sorted_packages.append(d)
+        except Exception:
+            pass
+    return sorted_packages
